f34d0a8c736a1a5e0d9dbdd0da6e76a81551b83d
Alexandre Joseph <jexhson@alexandrejoseph.com>
2012-07-28 14:26:55 +0200

    Use named imports

--- crack.py
+++ crack.py
@@ -18,7 +18,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 import string
-from _crack import *
+from _crack import FascistCheck, default_dictpath
 
 ascii_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 ascii_lowercase = "abcdefghijklmnopqrstuvwxyz"
