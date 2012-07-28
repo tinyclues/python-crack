f24340005ce0de817899edf0c4c252ff2ecc1b28
Alexandre Joseph <jexhson@alexandrejoseph.com>
2012-07-28 17:11:40 +0200

    Make some functions improvements

--- crack.py
+++ crack.py
@@ -17,11 +17,11 @@
 # along with Prua; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-import string
 from _crack import FascistCheck, default_dictpath
 
-ascii_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-ascii_lowercase = "abcdefghijklmnopqrstuvwxyz"
+digits = '0123456789'
+ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
 
 diff_ok = 5
 min_length = 9
@@ -32,6 +32,9 @@ oth_credit = 1
 
 
 def palindrome(s):
+    '''
+    Check if the string s is a palindrome
+    '''
     for i in xrange(len(s)):
         if s[i] != s[-i - 1]:
             return 0
@@ -102,7 +105,7 @@ def similar(old, new):
     characters in the new password are different from the old one or if the
     distance between them is acceptable
 
-    Accepted distance difference is defined by `diff_ok`, the number of
+    Accepted distance difference is defined by :diff_ok, the number of
     characters in the new password that must not be present in the old
     password.
     '''
@@ -117,13 +120,35 @@ def similar(old, new):
 
 
 def simple(new):
+    '''
+    Check that the password is not too simple and follow some criteria.
+
+    A minimum acceptable size is required (:min_length) for the new password
+    (plus one if credits are not disabled which is the default). In addition
+    to the number of characters in the new password, credit (of +1 in length)
+    is given for each different kind of character (digit, upper, lower and
+    other).
+
+    To set the minimum number of a character type that must be met for a new
+    password, credit of this type must be negative.
+
+    The default minimum length is 9 which is good for a old style Unix
+    password all of the same type of character but may be too low to exploit
+    the added security of a md5 system.
+
+    Note that there is a pair of length limits in cracklib itself, a "way too
+    short" limit of 4 which is hard coded in and a defined limit of 6 that
+    will be checked without reference to min_length. If you want to allow
+    passwords as short as 5 characters you should either not use this module
+    or recompile the crack library and then recompile this module.
+    '''
     digits = 0
     uppers = 0
     lowers = 0
     others = 0
 
     for c in new:
-        if c in string.digits:
+        if c in digits:
             digits = digits + 1
         elif c in ascii_uppercase:
             uppers = uppers + 1
@@ -132,44 +157,38 @@ def simple(new):
         else:
             others = others + 1
 
-    # The scam was this - a password of only one character type
-    # must be 8 letters long.  Two types, 7, and so on.
-    # This is now changed, the base size and the credits or defaults
-    # see the docs on the module for info on these parameters, the
-    # defaults cause the effect to be the same as before the change
-
-    if dig_credit >= 0 and digits > dig_credit:
+    if digits > dig_credit:
         digits = dig_credit
 
-    if up_credit >= 0 and uppers > up_credit:
+    if uppers > up_credit:
         uppers = up_credit
 
-    if low_credit >= 0 and lowers > low_credit:
+    if lowers > low_credit:
         lowers = low_credit
 
-    if oth_credit >= 0 and others > oth_credit:
+    if others > oth_credit:
         others = oth_credit
 
     size = min_length
 
     if dig_credit >= 0:
-        size = size - digits
-    elif digits < (dig_credit * -1):
+        size -= digits
+    elif digits < -dig_credit:
         return 1
 
     if up_credit >= 0:
-        size = size - uppers
-    elif uppers < (up_credit * -1):
+        size -= uppers
+    elif uppers < -up_credit:
         return 1
 
     if low_credit >= 0:
-        size = size - lowers
-    elif lowers < (low_credit * -1):
+        size -= lowers
+    elif lowers < -low_credit:
         return 1
 
     if oth_credit >= 0:
-        size = size - others
-    elif others < (oth_credit * -1):
+        size -= others
+    elif others < -oth_credit:
         return 1
 
     if len(new) < size:
